cc06dd6784a30bb9c8105c67dacb4f42a201472b
Vasiiy <vasya.smirnoff@mail.ru>
2023-01-27 15:20:06 +0300

    task 25 complete

---
+++ task25/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную 
+// степень B. Реализовать функцию возведения в степень 
+// самостоятельно!
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+int stepen (int osnovanie, int pokazatel)
+{
+    int count = 0;
+    int result = 1;
+    while (count < pokazatel)
+    {
+        result = result * osnovanie;
+        count++;
+    }
+    return result;
+}
+
+void main()
+{
+    Console.Write("Введите основание степени: ");
+    int number1 = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите показатель степени: ");
+    int number2 = int.Parse(Console.ReadLine()!);
+    int result = stepen(number1, number2);
+    Console.WriteLine($"{result}");
+}
+
+main();
+
